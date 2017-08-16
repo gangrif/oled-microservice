fdd071c67908971915000d573316bfccd113ce8c
Nate Lager <lagern@lafayette.edu>
2017-08-16 12:25:10 -0400

    updating flask import

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import ledfun as oled
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
-from flask.ext.jsonpify import jsonify
+from flask_jsonpify import jsonify
 
 # OLED init
 lcd = Winstar_GraphicOLED()
